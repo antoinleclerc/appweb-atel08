11f8d18512728b576bb55a756910b14e7f8c9fe1
Leclant <leclant.06@gmail.com>
2025-02-18 08:54:37 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import Timeline from "./components/Timeline.vue";
+</script>
+
+<template>
+  <div class="container">
+    <Timeline> </Timeline>
+  </div>
+</template>
+
+<style scoped></style>
